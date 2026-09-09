eccd0646d6a29e9ade678e02ca2ffa60e7ed02e3
Stuar-Truong2048 <stuar-truong2048@users.noreply.github.com>
2026-09-09 04:41:06 +0000

    feat: completed prime number checker

---
+++ build-a-prime-number-checker-module/index.js
@@ -0,0 +1,13 @@
+function isPrime(num) {
+  if (typeof num !== 'number' || num <= 1 || !Number.isInteger(num)) {
+    return false;
+  }
+  for (let i = 2; i <= Math.sqrt(num); i++) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+  return true;
+}
+
+module.exports = { isPrime };
